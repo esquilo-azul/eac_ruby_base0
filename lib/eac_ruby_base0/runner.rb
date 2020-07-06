67f4f773716b1566e31b32e343f959789ebb35f4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-07-06 19:07:54 -0300

    EacRubyBase0::Runner.

---
+++ lib/eac_ruby_base0/runner.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'eac_cli/default_runner'
+require 'eac_ruby_utils/core_ext'
+
+module EacRubyBase0
+  module Runner
+    require_sub __FILE__
+    common_concern do
+      include ::EacCli::DefaultRunner
+      runner_definition do
+        bool_opt '-q', '--quiet', 'Quiet mode.'
+        subcommands
+        alt do
+          bool_opt '-V', '--version', 'Show version.'
+        end
+      end
+    end
+
+    def run
+      on_speaker_node do |node|
+        node.stderr = ::StringIO.new if options.fetch('--quiet')
+        if options.fetch('--version')
+          show_version
+        else
+          run_with_subcommand
+        end
+      end
+    end
+
+    def show_version
+      out(context(:application).version + "\n")
+    end
+  end
+end
