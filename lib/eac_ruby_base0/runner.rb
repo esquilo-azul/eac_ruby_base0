5d6b9600273d800c6b51c664abf2eb0bb9c6dc79
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-10-18 18:42:51 -0300

    EacRubyBase0::Runner: option "--no-input".

--- lib/eac_ruby_base0/runner.rb
+++ lib/eac_ruby_base0/runner.rb
@@ -10,6 +10,7 @@ module EacRubyBase0
       include ::EacCli::DefaultRunner
       runner_definition do
         bool_opt '-q', '--quiet', 'Quiet mode.'
+        bool_opt '-I', '--no-input', 'Fail if a input is requested.'
         subcommands
         alt do
           bool_opt '-V', '--version', 'Show version.'
@@ -20,6 +21,7 @@ module EacRubyBase0
     def run
       on_speaker_node do |node|
         node.stderr = ::StringIO.new if options.fetch('--quiet')
+        node.stdin = FailIfRequestInput.new if options.fetch('--no-input')
         if options.fetch('--version')
           show_version
         else
@@ -34,6 +36,16 @@ module EacRubyBase0
 
     def show_version
       out("#{application_version}\n")
+    end
+
+    class FailIfRequestInput
+      enable_console_speaker
+
+      %w[gets noecho].each do |method|
+        define_method(method) do
+          raise "Input method requested (\"#{method}\") and option --no-input is set"
+        end
+      end
     end
   end
 end
