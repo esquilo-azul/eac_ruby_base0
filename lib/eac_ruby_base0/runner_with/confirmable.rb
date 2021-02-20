8fb68d9e69c4328ba55db1d7ba9702e8206f2d6a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-20 12:58:34 -0300

    EacRubyBase0::RunnerWith::Confirmable.

---
+++ lib/eac_ruby_base0/runner_with/confirmable.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'eac_cli/runner'
+require 'eac_ruby_utils/fs/traversable'
+
+module EacRubyBase0
+  module RunnerWith
+    module Confirmable
+      extend ::ActiveSupport::Concern
+
+      included do
+        include ::EacCli::Runner
+        runner_definition do
+          bool_opt '-c', '--confirm', 'Confirm changes.'
+        end
+      end
+
+      delegate :confirm?, to: :parsed
+    end
+  end
+end
