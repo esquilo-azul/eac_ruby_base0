2cc43473859aeac614cd9fd7b9849fd94d0c3905
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-21 13:30:19 -0300

    EacRubyBase0::RunnerWith::Confirmation: ask for confirmation.

--- lib/eac_ruby_base0/runner_with/confirmation.rb
+++ lib/eac_ruby_base0/runner_with/confirmation.rb
@@ -7,14 +7,30 @@ require 'eac_ruby_utils/fs/traversable'
 module EacRubyBase0
   module RunnerWith
     module Confirmation
+      DEFAULT_CONFIRM_QUESTION_TEXT = 'Confirm?'
+
       common_concern do
         include ::EacCli::Runner
+        enable_settings_provider
         runner_definition do
-          bool_opt '-c', '--confirm', 'Confirm changes.'
+          bool_opt '--no', 'Deny confirmation without question.'
+          bool_opt '--yes', 'Accept confirmation without question.'
         end
       end
 
-      delegate :confirm?, to: :parsed
+      def confirm?(message = nil)
+        return false if parsed.no?
+        return true if parsed.yes?
+
+        request_input(
+          message || setting_value(:confirm_question_text, default: DEFAULT_CONFIRM_QUESTION_TEXT),
+          bool: true
+        )
+      end
+
+      def run_confirm(message = nil)
+        yield if confirm?(message)
+      end
     end
   end
 end
