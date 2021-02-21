9f877efa0381ba3cfa908ec49220efe6246ce9a5
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-02-21 13:33:43 -0300

    EacRubyBase0::RunnerWith::Confirmation: refact with EacRubyUtils::CommonConcern.

--- lib/eac_ruby_base0/runner_with/confirmation.rb
+++ lib/eac_ruby_base0/runner_with/confirmation.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 require 'eac_cli/runner'
+require 'eac_ruby_utils/core_ext'
 require 'eac_ruby_utils/fs/traversable'
 
 module EacRubyBase0
   module RunnerWith
     module Confirmation
-      extend ::ActiveSupport::Concern
-
-      included do
+      common_concern do
         include ::EacCli::Runner
         runner_definition do
           bool_opt '-c', '--confirm', 'Confirm changes.'
