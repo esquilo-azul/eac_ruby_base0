645a4beb442b68c07d882be921ca9d4348141c03
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-11 15:40:29 -0300

    EacRubyBase0::ApplicationXdg: fix missing requires.

--- lib/eac_ruby_base0/application_xdg.rb
+++ lib/eac_ruby_base0/application_xdg.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'eac_ruby_utils/core_ext'
+
 module EacRubyBase0
   # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
   class ApplicationXdg
